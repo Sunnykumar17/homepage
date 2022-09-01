82a288be5fe0a0c1a78bfa33f27ea3735f7c0634
Sunnykumar17 <98548654+Sunnykumar17@users.noreply.github.com>
2022-09-01 16:45:53 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ const Contact = _ => {
     return (
         <section>
             <h3>🤙 Contact Me</h3>
-            <a href="https://www.linkedin.com/in/sunny-kumar-09b98a226/"target="_blank">LinkedIn</a>
-            <a href="https://github.com/Sunnykumar17/" target="_blank">Github</a>
-            <a href="https://www.hackerrank.com/sk6328679" target="_blank">Hackerrank</a>
-            <a href="https://leetcode.com/sunny_kumar17/"target="_blank">Leetcode</a>
+            <a href="https://www.linkedin.com/in/sunny-kumar-09b98a226/"target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/Sunnykumar17/" target="_blank" rel="noreferrer">Github</a>
+            <a href="https://www.hackerrank.com/sk6328679" target="_blank" rel="noreferrer">Hackerrank</a>
+            <a href="https://leetcode.com/sunny_kumar17/"target="_blank" rel="noreferrer">Leetcode</a>
         </section>
        
     )
